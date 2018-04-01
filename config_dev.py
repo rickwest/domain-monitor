941b5673ecb4c246ef720320869ac84f40c0e338
Rick <rickwestdev@gmail.com>
2018-04-01 20:08:39 +0100

    Add tld's and business's entity endings to dev and production config

--- config_dev.py
+++ config_dev.py
@@ -1,6 +1,5 @@
-COMMON_NAME_ENDINGS = [
-    'law',
-    'legal',
-    'lawyer',
-    'solicitor',
-]
+COMMON_NAME_ENDINGS = ['law', 'solicitors', 'llp']
+
+TLDS = ['.com', '.co.uk']
+
+BUSINESS_ENTITY_ENDINGS = ['ltd', 'limited']
