a82b24302a1e5958e9908d458875c0e90c958662
Rick <rickwestdev@gmail.com>
2018-04-02 14:35:35 +0100

    For now, bring views back into app.py for simplicity

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+import click
+import csv
 from flask import Flask
 from peewee import *
-from domain_monitor import set_common_name_endings, set_business_entity_endings, set_tlds
+from domain_monitor import *
 
 app = Flask(__name__)
 app.config.from_envvar('APP_CONFIG')
@@ -39,4 +41,33 @@ set_tlds(app.config['TLDS'])
 # create the tables. By default, Peewee will determine if the tables already exist, and conditionally create them
 db.create_tables([Firm])
 
-import views
+
+@app.cli.command()
+def import_clc_firms():
+    """Simple program that parses data from Council for Licensed Conveyancers list of firms and inserts into database table. """
+    filename = 'raw-firm-data.csv'
+    with open(filename, newline='', mode='r') as f:
+        # skip the first entry as just column headers
+        reader = csv.reader(f)
+        next(reader)
+        with click.progressbar(reader, length=1000) as bar:
+            for row in bar:
+                if len(row) == 2:
+                    firm_name = row[0].lower()
+                    email_address = row[1]
+                    known_domain = None
+                    if email_address is not '':
+                        known_domain = get_domain_from_email_address(email_address)
+                    Firm.get_or_create(firm_name=firm_name,
+                                       email_address=email_address,
+                                       defaults={'known_domain': known_domain})
+        click.echo('Database seeded successfully')
+
+
+@app.cli.command()
+def check_all_domains():
+    firms = Firm.select()
+
+    with click.progressbar(firms) as bar:
+        check_domains(bar)
+    click.echo('All domains checked successfully')
